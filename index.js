79fd6bb44f37628d07ac55b770c22648f027223b
Tamiris Vieira <85970510+TamirisVieira@users.noreply.github.com>
2022-12-01 11:32:38 +0000

    migração do banco no cadastro e listar

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 var cookieParser = require('cookie-parser');
 
 const express = require('express');
-const { produto } = require('./models');
+const { produtos } = require('./models');
 
 const app = express();
 
@@ -13,30 +13,30 @@ app.set('view engine', 'ejs');
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(cookieParser());
 
-app.get('/listar', async function (req,res){
-  const produtos = await produto.findAll();
-  res.json(produtos);
+app.get('/listar', async function(req, res) {
+  const produto = await produtos.findAll();
+  res.json(produto);
 })
 
-app.get('/cadastro', async function (req,res){
+app.get('/cadastro', async function(req, res) {
   res.render("cadastro")
 })
 
-app.post('/cadastro', async function (req,res){
-  const produto_ = await produto.create(req.body)
+app.post('/cadastro', async function(req, res) {
+  const produto_ = produtos.create(req.body)
   res.json(produto_)
 })
 
-app.get('/', async function(req, res){
+app.get('/', async function(req, res) {
   res.render("home")
 })
 
 
-app.listen(3000, function() {
-  console.log('App de Exemplo escutando na porta 3000!')
+app.listen(6000, function() {
+  console.log('App de Exemplo escutando na porta 6000!')
 });
